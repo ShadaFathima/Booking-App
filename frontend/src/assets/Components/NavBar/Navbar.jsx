64256202eeb481c4481f4d819f32b8270526d828
jawadnm <jawadnm2369@gmail.com>
2024-01-25 12:35:03 +0530

    menu

--- frontend/src/assets/Components/NavBar/Navbar.jsx
+++ frontend/src/assets/Components/NavBar/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
           aria-controls="navbar-default"
           aria-expanded="false"
         >
-          <span className="sr-only">Open main menu</span>
+          <span className="sr-only">Open main menu...</span>
           <svg
             className="w-5 h-5"
             aria-hidden="true"
